d81fa7597488ea176a7908137f55279494355e1d
Miguel Guthridge <hdsq@outlook.com.au>
2023-05-04 14:13:37 +1000

    Basic server route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+
+dotenv.config();
+const port = parseInt(process.env.PORT as string);
+const ip = process.env.IP as string;
 
 const app = express();
 app.use(cors());
+app.use(morgan('dev'));
 
 
-app.listen(() => console.log('App started!'));
+app.get('/', (req, res) => {
+  res.send('Prerequisite backend is up and running!');
+});
+
+app.listen(
+  port,
+  ip,
+  () => console.log(`Prerequisite backend running on http://${ip}:${port}/`)
+);
