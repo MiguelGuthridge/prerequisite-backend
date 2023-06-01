520c1b3a676a36a3e1a77ee308735cef543d7b2f
Miguel Guthridge <hdsq@outlook.com.au>
2023-06-01 23:19:20 +1000

    Separate server code from server runner

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,9 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import { expressjwt as jwt } from 'express-jwt';
-import errorHandler from 'middleware-http-errors';
-import debug from './routes/debug';
-import auth from './routes/auth';
-import projects from './routes/projects';
-
-dotenv.config();
+import server from './server';
 
 const port = parseInt(process.env.PORT as string);
 const ip = process.env.IP as string;
 
-const app = express();
-app.use(json());
-app.use(cors());
-app.use(morgan('dev'));
-
-app.use(
-  jwt({
-    secret: process.env.ACCESS_TOKEN_SECRET as string,
-    algorithms: ['HS256'],
-  }).unless({
-    path: [
-      '/',
-      /\/debug\/*/,
-      '/auth/register',
-      '/auth/login',
-    ],
-  })
-);
-
-app.get('/', (req, res) => {
-  res.send('Prerequisite backend is up and running!');
-});
-
-app.use('/debug', debug);
-app.use('/auth', auth);
-app.use('/projects', projects);
-
-app.use(errorHandler());
-
-app.listen(
+server.listen(
   port,
   ip,
   () => console.log(`Prerequisite backend running on http://${ip}:${port}/`)
