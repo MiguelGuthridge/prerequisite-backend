6fd07dfc3e02e399689569453a1c1e8f45384915
Miguel Guthridge <hdsq@outlook.com.au>
2023-06-06 00:53:42 +1000

    Add error case tests for auth/register

--- tests/auth/register.test.ts
+++ tests/auth/register.test.ts
@@ -8,4 +8,31 @@ describe('/auth/register', () => {
     expect(id).toBeDefined();
     expect(token).toBeDefined();
   });
+
+  describe('error cases', () => {
+    it('fails to register users with weak passwords', async () => {
+      await expect(
+        api.auth.register('myusername', 'Display Name', 'abc123')
+      ).rejects.toMatchObject({ code: 400 });
+    });
+
+    it('fails to register users with duplicate usernames', async () => {
+      await api.auth.register('myusername', 'Display Name', 'abc123ABC!');
+      await expect(
+        api.auth.register('myusername', 'Other Name', 'abc123ABC!')
+      ).rejects.toMatchObject({ code: 400 });
+    });
+
+    it.each([
+      { username: 'User', explanation: 'Capital letters' },
+      { username: 'user!', explanation: 'Punctuation' },
+      { username: 'my username', explanation: 'whitespace' },
+    ])(
+      'fails to register users with bad usernames ($explanation)',
+      async ({ username }) => {
+        await expect(
+          api.auth.register(username, 'Other Name', 'abc123ABC!')
+        ).rejects.toMatchObject({ code: 400 });
+      });
+  });
 });
