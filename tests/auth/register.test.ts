874f8b4ffd890a1858f6318df2dcf30112dd91c6
Miguel Guthridge <hdsq@outlook.com.au>
2023-06-06 00:07:00 +1000

    Fix failing test

--- tests/auth/register.test.ts
+++ tests/auth/register.test.ts
@@ -1,8 +1,10 @@
 import api from '../api';
 
+beforeEach(api.debug.clear);
+
 describe('/auth/register', () => {
   it('allows users to register', async () => {
-    const { id, token } = await api.auth.register('myusername', 'Display Name', 'abc123ABC');
+    const { id, token } = await api.auth.register('myusername', 'Display Name', 'abc123ABC!');
     expect(id).toBeDefined();
     expect(token).toBeDefined();
   });
