63afc9ce54b4267e85e072f7804ef41051b6192b
Miguel Guthridge <hdsq@outlook.com.au>
2023-06-09 00:50:11 +1000

    Add route to view project details

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -4,11 +4,13 @@ import { getUserById } from '../data/users';
 import HttpError from 'http-errors';
 import { UserId } from '../types/user';
 import { ProjectId } from '../types/project';
-import { getVisibleProjects } from '../data/projects';
+import { v4 as uuid } from 'uuid';
+import { getProjectById, getVisibleProjects, isProjectVisibleToUser } from '../data/projects';
+import { getUserIdFromRequest as userFromToken } from '../util/token';
 
 const projects = Router();
 
-projects.post('/create', (req, res) => {
+projects.post('/', (req, res) => {
   const {
     name,
     description,
@@ -20,7 +22,7 @@ projects.post('/create', (req, res) => {
   }
 
   // Generate project ID
-  const id = '1' as ProjectId; // FIXME
+  const id = uuid() as ProjectId;
 
   getData().projects[id] = {
     id,
@@ -32,9 +34,26 @@ projects.post('/create', (req, res) => {
   res.json({ id });
 });
 
-projects.get('/list', (req, res) => {
-  const { id } = req.query as { id: UserId };
+projects.get('/', (req, res) => {
+  const id = userFromToken(req);
   res.json({ projects: getVisibleProjects(id) });
+});
+
+projects.get('/:projectId', (req, res) => {
+  const id = userFromToken(req);
+  const projectId = req.params.projectId as ProjectId;
+
+  const project = getProjectById(projectId);
+
+  if (project === null) {
+    throw HttpError(400, 'Project does not exist');
+  }
+
+  if (!isProjectVisibleToUser(id, projectId)) {
+    throw HttpError(403, 'Unable to view project');
+  }
+
+  res.json(project);
 });
 
 export default projects;
