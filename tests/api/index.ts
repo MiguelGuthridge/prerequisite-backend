354f49864af8939d713eb1566104bc1898938f6e
Miguel Guthridge <hdsq@outlook.com.au>
2023-06-19 23:23:24 +1000

    As per usual, I forgot to export it from the api module

--- tests/api/index.ts
+++ tests/api/index.ts
@@ -1,9 +1,11 @@
 import debug from './debug';
 import auth from './auth';
 import project from './project';
+import task from './task';
 
 export default {
   debug,
   auth,
   project,
+  task,
 };
