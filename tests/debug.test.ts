704baa0ab1618a866b7ffdedc3b3d31cc2a7f128
Miguel Guthridge <hdsq@outlook.com.au>
2023-06-21 23:45:33 +1000

    Remove unneeded TODO comment

--- tests/debug.test.ts
+++ tests/debug.test.ts
@@ -3,7 +3,6 @@ import api from './api';
 describe('/debug/echo', () => {
   it('echoes the given content', async () => {
     const data = await api.debug.echo('hello');
-    // INVESTIGATE: Why does .toStrictEqual not work here?
     expect(data).toStrictEqual({ value: 'hello' });
   });
 });
