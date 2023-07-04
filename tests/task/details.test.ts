5090e0859ce70520a60800f3e982e8bdfee4f42e
Miguel Guthridge <hdsq@outlook.com.au>
2023-07-04 23:52:47 +1000

    Add tests for task details

--- tests/task/details.test.ts
+++ tests/task/details.test.ts
@@ -1,13 +1,60 @@
+import { TaskId } from '../../src/types/task';
+import { Token } from '../../src/types/user';
 import api from '../api';
+import { makeProject, makeTask, makeUser } from '../helpers';
 
 beforeEach(api.debug.clear);
 
 describe('/task details', () => {
-  it.todo('gets the details for a task');
+  it('gets the details for a task', async () => {
+    const { token } = await makeUser();
+    const { id: projectId } = await makeProject(token);
 
-  it.todo('fails for invalid tokens');
+    const { id: taskId } = await api.task.create(
+      token,
+      projectId,
+      'my task',
+      '',
+      false,
+      [],
+    );
 
-  it.todo('fails for invalid task IDs');
+    // Details should be correct
+    await expect(api.task.details(token, taskId)).resolves.toStrictEqual({
+      id: taskId,
+      name: 'my task',
+      description: '',
+      complete: false,
+      prerequisites: [],
+      project: projectId,
+    });
+  });
 
-  it.todo('fails for tasks the user cannot see');
+  it('fails for invalid tokens', async () => {
+    const { token } = await makeUser();
+    const { id: projectId } = await makeProject(token);
+    const { id: taskId } = await makeTask(token, projectId);
+
+    await expect(api.task.details('bad' as Token, taskId)).rejects.toMatchObject({
+      code: 401,
+    });
+  });
+
+  it('fails for invalid task IDs', async () => {
+    const { token } = await makeUser();
+    await expect(api.task.details(token, 'bad' as TaskId)).rejects.toMatchObject({
+      code: 400,
+    });
+  });
+
+  it('fails for tasks the user cannot see', async () => {
+    const { token } = await makeUser();
+    const { id: projectId } = await makeProject(token);
+    const { id: taskId } = await makeTask(token, projectId);
+
+    const { token: token2 } = await makeUser(2);
+    await expect(api.task.details(token2, taskId)).rejects.toMatchObject({
+      code: 403,
+    });
+  });
 });
