bc2d37e0bc785facac6c1f256c8ac92bbb528ad4
Miguel Guthridge <hdsq@outlook.com.au>
2023-07-05 15:19:30 +1000

    Add basic tests for task deletion

--- tests/task/remove.test.ts
+++ tests/task/remove.test.ts
@@ -1,15 +1,63 @@
+import { TaskDeletionStrategy, TaskId } from '../../src/types/task';
+import { Token } from '../../src/types/user';
 import api from '../api';
+import { makeProject, makeTask, makeUser } from '../helpers';
 
 beforeEach(api.debug.clear);
 
 describe('/task remove', () => {
-  it.todo('deletes tasks');
+  it('deletes tasks', async () => {
+    const { token } = await makeUser();
+    const { id: projectId } = await makeProject(token);
+    const { id: taskId } = await makeTask(token, projectId);
 
-  it.todo('fails for invalid task ids');
+    await expect(
+      api.task.remove(token, taskId, TaskDeletionStrategy.Cascade)
+    ).resolves.toStrictEqual(
+      {}
+    );
 
-  it.todo('fails for users without ownership of the project');
+    // Make sure the task was removed from the project
+    await expect(
+      api.project.details(token, projectId)
+    ).resolves.toMatchObject({
+      tasks: [],
+    });
+  });
 
-  it.todo('fails for invalid tokens');
+  it('fails for invalid task ids', async () => {
+    const { token } = await makeUser();
+    await expect(
+      api.task.remove(token, 'bad' as TaskId, TaskDeletionStrategy.Cascade)
+    ).rejects.toMatchObject({
+      code: 400,
+    });
+  });
+
+  it('fails for users without ownership of the project', async () => {
+    const { token } = await makeUser();
+    const { id: projectId } = await makeProject(token);
+    const { id: taskId } = await makeTask(token, projectId);
+
+    const { token: token2 } = await makeUser(2);
+    await expect(
+      api.task.remove(token2, taskId, TaskDeletionStrategy.Cascade)
+    ).rejects.toMatchObject({
+      code: 403,
+    });
+  });
+
+  it('fails for invalid tokens', async () => {
+    const { token } = await makeUser();
+    const { id: projectId } = await makeProject(token);
+    const { id: taskId } = await makeTask(token, projectId);
+
+    await expect(
+      api.task.remove('bad' as Token, taskId, TaskDeletionStrategy.Cascade)
+    ).rejects.toMatchObject({
+      code: 401,
+    });
+  });
 
   describe('deletion strategies', () => {
     describe('cascade', () => {
