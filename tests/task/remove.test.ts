8ace71e3afb5a485ecc5daf4c29e98a30f171e67
Miguel Guthridge <hdsq@outlook.com.au>
2023-07-05 15:59:50 +1000

    Add tests for task removal strategies

--- tests/task/remove.test.ts
+++ tests/task/remove.test.ts
@@ -6,7 +6,7 @@ import { makeProject, makeTask, makeUser } from '../helpers';
 beforeEach(api.debug.clear);
 
 describe('/task remove', () => {
-  it('deletes tasks', async () => {
+  it('removes tasks', async () => {
     const { token } = await makeUser();
     const { id: projectId } = await makeProject(token);
     const { id: taskId } = await makeTask(token, projectId);
@@ -59,19 +59,116 @@ describe('/task remove', () => {
     });
   });
 
-  describe('deletion strategies', () => {
+  describe('removal strategies', () => {
     describe('cascade', () => {
-      it.todo('deletes all tasks that depend on it');
+      it('removes all tasks that depend on it', async () => {
+        const { token } = await makeUser();
+        const { id: projectId } = await makeProject(token);
+        const { id: taskId1 } = await makeTask(token, projectId);
+        const { id: taskId2 } = await makeTask(
+          token,
+          projectId,
+          {
+            num: 2,
+            prerequisites: [taskId1],
+          }
+        );
+
+        // Delete task 1 with cascade strategy
+        await expect(
+          api.task.remove(token, taskId1, TaskDeletionStrategy.Cascade)
+        ).resolves.toStrictEqual(
+          {}
+        );
+
+        // Task 2 should also be deleted
+        await expect(
+          api.task.details(token, taskId2)
+        ).rejects.toMatchObject({
+          code: 400,
+        });
+      });
     });
 
     describe('reroute', () => {
-      it.todo('updates prerequisites for direct dependents');
+      it('updates prerequisites for direct dependents', async () => {
+        const { token } = await makeUser();
+        const { id: projectId } = await makeProject(token);
+        const { id: taskId1 } = await makeTask(token, projectId);
+        const { id: taskId2 } = await makeTask(
+          token,
+          projectId,
+          {
+            num: 2,
+            prerequisites: [taskId1],
+          }
+        );
+        const { id: taskId3 } = await makeTask(
+          token,
+          projectId,
+          {
+            num: 3,
+            prerequisites: [taskId2],
+          }
+        );
+
+        // Delete task 2 with reroute strategy
+        await expect(
+          api.task.remove(token, taskId2, TaskDeletionStrategy.Cascade)
+        ).resolves.toStrictEqual(
+          {}
+        );
+
+        // Task 3 should now have task 1 as a prerequisite
+        await expect(
+          api.task.details(token, taskId3)
+        ).resolves.toMatchObject({
+          prerequisites: [taskId1],
+        });
+      });
     });
 
     describe('trim', () => {
-      it.todo('removes task as a prerequisite for all direct dependents');
+      it('removes task as a prerequisite for all direct dependents', async () => {
+        const { token } = await makeUser();
+        const { id: projectId } = await makeProject(token);
+        const { id: taskId1 } = await makeTask(token, projectId);
+        const { id: taskId2 } = await makeTask(
+          token,
+          projectId,
+          {
+            num: 2,
+            prerequisites: [taskId1],
+          }
+        );
+
+        // Delete task 1 with trim strategy
+        await expect(
+          api.task.remove(token, taskId1, TaskDeletionStrategy.Trim)
+        ).resolves.toStrictEqual(
+          {}
+        );
+
+        // Task 2 should have no more prerequisites
+        await expect(
+          api.task.details(token, taskId2)
+        ).resolves.toMatchObject({
+          prerequisites: [],
+        });
+      });
     });
 
-    it.todo('fails for non-existent strategies');
+    it('fails for non-existent strategies', async () => {
+      const { token } = await makeUser();
+      const { id: projectId } = await makeProject(token);
+      const { id: taskId } = await makeTask(token, projectId);
+
+      await expect(
+        // Yucky hack
+        api.task.remove(token, taskId, -1 as unknown as TaskDeletionStrategy)
+      ).rejects.toMatchObject(
+        { code: 400 }
+      );
+    });
   });
 });
